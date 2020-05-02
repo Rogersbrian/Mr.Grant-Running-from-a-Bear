230472a0c55d22a366a06f8cc7025f68d99b3fd0
Rogersbrian <64108730+Rogersbrian@users.noreply.github.com>
2020-05-02 16:19:56 -0700

    Update Soda.java

--- src/Soda.java
+++ src/Soda.java
@@ -12,12 +12,16 @@ public class Soda extends Actor
      * Act - do whatever the Soda wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    int count=0;
+    private int count=0;
     public void act() 
     {
+        count++;
+        if(count>800)
+        {
         int y=getY();
         int x=getX();
        x-=2;
         setLocation(x,y);
     }     
+}
 }

90c14e6c20c3c81376b347f2c8ef86eb64ece434
Rogersbrian <64108730+Rogersbrian@users.noreply.github.com>
2020-05-02 16:20:27 -0700

    Update Tumbleweed.java

--- src/Tumbleweed.java
+++ src/Tumbleweed.java
@@ -12,12 +12,17 @@ public class Tumbleweed extends Actor
      * Act - do whatever the Tumbleweed wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int count=0;
     public void act() 
     {
+        count++;
+        if(count>550)
+        {
          int x= getX();
 double amplitude = 2.0;
 double frequency = 5.0;
 x-=3;
 setLocation(x, getY()+(int)(Math.sin((double)(getX()+1)*frequency/95.5)*amplitude));
     }    
+}
 }

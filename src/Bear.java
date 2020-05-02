968c0c25f38390c9f64821f613fe4e36a8707f85
Rogersbrian <64108730+Rogersbrian@users.noreply.github.com>
2020-05-02 16:16:27 -0700

    Update Bear.java

--- src/Bear.java
+++ src/Bear.java
@@ -1,19 +1,28 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Bear here.
+ * Write a description of class Bird here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Bear extends Actor
+public class Bird extends Actor
 {
     /**
-     * Act - do whatever the Bear wants to do. This method is called whenever
+     * Act - do whatever the Bird wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int count=0;
     public void act() 
     {
-        // Add your action code here.
+        count++;
+    if(count>500)
+    {
+        int x= getX();
+double amplitude = 2.0;
+double frequency = 3.0;
+x--;
+setLocation(x, getY()+(int)(Math.sin((double)(getX()+1)*frequency/95.5)*amplitude));
     }    
+}
 }

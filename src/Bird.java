1ac8a706c4d4d23e535886fd212dfed6a4ff2757
Rogersbrian <64108730+Rogersbrian@users.noreply.github.com>
2020-05-09 22:43:44 -0700

    Update Bird.java

--- src/Bird.java
+++ src/Bird.java
@@ -12,12 +12,28 @@ public class Bird extends Actor
      * Act - do whatever the Bird wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int count=0;
+    private int rate=500;
     public void act() 
     {
-       int x= getX();
-double amplitude = 2.0;
-double frequency = 3.0;
-x--;
-setLocation(x, getY()+(int)(Math.sin((double)(getX()+1)*frequency/95.5)*amplitude));
+    count++;
+    if(count>rate)
+        {
+        move();
+        }  
+        if(getX()==0)
+       {
+       count=0;
+       setLocation(600, 190);
+       if(rate>95)
+       rate--;
+       }
     }    
+    public void move(){
+    int x= getX();
+    double amplitude = 2.0;
+    double frequency = 3.0;
+    x--;
+    setLocation(x, getY()+(int)(Math.sin((double)(getX()+1)*frequency/95.5)*amplitude));
+}
 }

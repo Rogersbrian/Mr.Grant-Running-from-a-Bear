99c96bfa682d0602c8ff74ee75531421065a6730
Rogersbrian <64108730+Rogersbrian@users.noreply.github.com>
2020-05-18 17:32:39 -0700

    Update MyWorld.java

--- src/MyWorld.java
+++ src/MyWorld.java
@@ -31,29 +31,24 @@ public class MyWorld extends World
     addObject(new RoadFront(), 599, height);
     addObject(new Bear(), 100, 170);
     addObject(new Jumper(), 245, 170);
-    addObject(new Healthbar(), 500, UI_top);
     addObject(new Timer(), 200, 30);
     addObject(new Cactus(), 600, 185);
     addObject(new Bird(), 600, 100);
     addObject(new Soda(), 600, 180);
     addObject(new Tumbleweed(), 600, 190);
-    addObject(new Left() , 520, 200);
-    addObject(new Right(), 525, 200);
-    
-    
-    /*addObject(new Scorpion(), 600, 190);
+    addObject(new Left() , 490, 200);
+    addObject(new Right(), 495, 200);
+    addObject(new Scorpion(), 600, 190);
     addObject(new Gatorade(), 600, 180);
     addObject(new ZPizza(), 600, 190);
     addObject(new Labtop(), 600, 170);
     addObject(new ElectricBike(), 600, 180);
     addObject(new CliffBar(), 600, 170);
-*/
+    addObject(new Points(), 60, 30);
+    addObject(new Block(), 620, 250);
 }
      public static Pacer getPacer() {
         return pacer;
         
         }   
     }
-    
-    
-
